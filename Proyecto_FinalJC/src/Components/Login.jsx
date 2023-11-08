ef7cc3b337433e36e94ac8fdf4ed1b7a321c61b9
Leimar <leimarxxx7@gmail.com>
2023-11-08 06:28:16 -0500

    Se crea el repositorio, del proyecto finalJC

---
+++ Proyecto_FinalJC/src/Components/Login.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+
+Login.propTypes = {
+
+};
+
+
+export default Login;
